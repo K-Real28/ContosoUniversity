2f5100f3d512ea93a0d520452549e9928c4ac0ff
Kiril <Kiril@PC-KIRILL>
2022-11-12 14:02:16 +0200

    Добавьте файлы проекта.

---
+++ ContosoUniversity/ViewModels/StudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
